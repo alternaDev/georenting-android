8e6993a37f24bef560cb25992ee5e26b8950d0e3
Jan-Henrik Bruhn <jh.bruhn@me.com>
2015-07-28 17:03:31 +0200

    Added Stetho.

--- app/src/main/java/de/alternadev/georenting/GeoRentingApplication.java
+++ app/src/main/java/de/alternadev/georenting/GeoRentingApplication.java
@@ -2,6 +2,7 @@ package de.alternadev.georenting;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
 import com.squareup.leakcanary.LeakCanary;
 
 import timber.log.Timber;
@@ -15,6 +16,16 @@ public class GeoRentingApplication extends Application {
 
         if(BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
+        }
+
+        if(BuildConfig.DEBUG) {
+            Stetho.initialize(
+                    Stetho.newInitializerBuilder(this)
+                            .enableDumpapp(
+                                    Stetho.defaultDumperPluginsProvider(this))
+                            .enableWebKitInspector(
+                                    Stetho.defaultInspectorModulesProvider(this))
+                            .build());
         }
 
         Timber.d("GeoRenting started.");
