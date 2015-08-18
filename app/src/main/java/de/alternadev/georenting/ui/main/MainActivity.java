b7f95134eab7ebd0450a0073568df71217e19deb
Jan-Henrik Bruhn <jh.bruhn@me.com>
2015-08-18 13:28:37 +0200

    Added some testing things for a main activity.

---
+++ app/src/main/java/de/alternadev/georenting/ui/main/MainActivity.java
@@ -0,0 +1,17 @@
+package de.alternadev.georenting.ui.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import de.alternadev.georenting.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
