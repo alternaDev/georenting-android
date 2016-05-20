4fd6ff8b917a7d0b20ae5d262a8a4da4088bbe97
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-05-20 14:40:52 +0200

    Removed unused import.

--- app/src/production/java/de/alternadev/georenting/GeoRentingComponent.java
+++ app/src/production/java/de/alternadev/georenting/GeoRentingComponent.java
@@ -8,7 +8,6 @@ import de.alternadev.georenting.data.NetworkModule;
 import de.alternadev.georenting.data.DataModule;
 import de.alternadev.georenting.data.api.ApiModule;
 import de.alternadev.georenting.data.api.ProductionApiModule;
-import de.alternadev.georenting.data.api.fcm.GcmRegistrationIntentService;
 import de.alternadev.georenting.data.auth.AuthModule;
 import de.alternadev.georenting.ui.SignInActivity;
 import de.alternadev.georenting.ui.main.MainActivity;
