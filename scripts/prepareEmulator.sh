331b59f3bee4ff019ca197c05a0e7cc7e3b39afd
Jan-Henrik Bruhn <jh.bruhn97@gmail.com>
2016-05-29 20:49:33 +0200

    Update prepareEmulator.sh

--- scripts/prepareEmulator.sh
+++ scripts/prepareEmulator.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo no | android create avd --force -n test -t android-21 --abi "google_apis/armeabi-v7a"
-emulator -avd test -no-window &
+emulator -avd test -no-window -no-audio &
 android-wait-for-emulator
+- adb shell input keyevent 82 &
