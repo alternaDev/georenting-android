b0f43ddf299c17185e797240ce871c2e96abafff
Jan-Henrik Bruhn <jh.bruhn97@gmail.com>
2016-05-29 17:34:23 +0200

    Update prepareEmulator.sh

--- scripts/prepareEmulator.sh
+++ scripts/prepareEmulator.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo no | android create avd --force -n test -t android-23 --abi "google_apis/armeabi-v7a"
+echo no | android create avd --force -n test -t android-23 --abi "google_apis/x86"
 emulator -avd test -no-window &
 android-wait-for-emulator
