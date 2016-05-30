05be92be85db523b6a979270a685c30f8802bdea
Jan-Henrik Bruhn <jh.bruhn97@gmail.com>
2016-05-30 10:16:55 +0200

    Update Local geoFences ever 30 Minutes.

--- app/src/main/java/de/alternadev/georenting/data/tasks/UpdateGeofencesTask.java
+++ app/src/main/java/de/alternadev/georenting/data/tasks/UpdateGeofencesTask.java
@@ -262,7 +262,7 @@ public class UpdateGeofencesTask extends GcmTaskService {
         Task task = new PeriodicTask.Builder()
                 .setRequiredNetwork(PeriodicTask.NETWORK_STATE_CONNECTED)
                 .setService(UpdateGeofencesTask.class)
-                .setPeriod(60 * 60) // Every 60 Minutes.
+                .setPeriod(30 * 60) // Every 30 Minutes.
                 .setFlex(30)
                 .setUpdateCurrent(true)
                 .setTag("GeofenceUpdater")
