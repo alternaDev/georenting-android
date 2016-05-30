2438dbd35f48299c209a2ae2c879230eb038b88b
Jan-Henrik Bruhn <jh.bruhn97@gmail.com>
2016-05-30 13:52:08 +0200

    Added no skin parameter to Android Emulator

--- scripts/prepareEmulator.sh
+++ scripts/prepareEmulator.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo no | android create avd --force -n test -t android-21 --abi "google_apis/armeabi-v7a"
-emulator -avd test -no-window -no-audio &
+echo no | android create avd --force -n test -t android-22 --abi "google_apis/armeabi-v7a"
+emulator -avd test -no-skin -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82 &
