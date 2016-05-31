033de5330243bcc46f5c915db8846bb3fa30c42e
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-05-31 08:43:20 +0200

    Switched back to ARM images.

--- scripts/prepareEmulator.sh
+++ scripts/prepareEmulator.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo no | android create avd --force -n test -t android-22 --abi "google_apis/x86"
+echo no | android create avd --force -n test -t android-22 --abi "google_apis/armeabi-v7a"
 emulator -avd test -no-skin -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82 &
