23dddfd835e1dd1fdd88eb7cb1f5ef9ab655dd91
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-06-09 00:18:14 +0200

    Switched to Firebase Auth.

--- app/src/main/java/de/alternadev/georenting/data/auth/GoogleAuth.java
+++ app/src/main/java/de/alternadev/georenting/data/auth/GoogleAuth.java
@@ -1,16 +1,27 @@
 package de.alternadev.georenting.data.auth;
 
 import android.app.Application;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GetTokenResult;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.Date;
 
@@ -21,6 +32,8 @@ import de.alternadev.georenting.R;
 import de.alternadev.georenting.data.api.GeoRentingService;
 import de.alternadev.georenting.data.api.model.SessionToken;
 import de.alternadev.georenting.data.api.model.User;
+import de.alternadev.georenting.ui.SignInActivity;
+import hugo.weaving.DebugLog;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
@@ -42,39 +55,62 @@ public class GoogleAuth {
     @Inject
     SharedPreferences mPreferences;
 
+    private FirebaseAuth mAuth;
+
     public GoogleAuth(Application application) {
         ((GeoRentingApplication) application).getComponent().inject(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public OptionalPendingResult<GoogleSignInResult> getAuthTokenSilent(GoogleApiClient apiClient) {
         return Auth.GoogleSignInApi.silentSignIn(apiClient);
     }
 
+    @DebugLog
     public Observable<SessionToken> handleSignIn(GoogleSignInResult result) {
         if(result.isSuccess() && result.getSignInAccount() != null) {
-            Timber.d(result.getSignInAccount().toString());
+            return Observable.create(subscriber -> {
+                subscriber.onStart();
+                firebaseAuthWithGoogle(result.getSignInAccount()).addOnSuccessListener(authResult -> {
+                    Timber.d("Got Auth REsult!");
 
-            return mGeoRentingService.auth(new User(result.getSignInAccount().getServerAuthCode()))
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .map(sessionToken -> {
-                        if(sessionToken.token == null || sessionToken.token.equals("")) {
-                            return sessionToken;
-                        }
+                    authResult.getUser().getToken(false).addOnSuccessListener(getTokenResult -> {
+                        Timber.d("Got Token REsult:%s", getTokenResult.getToken());
 
-                        mPreferences.edit()
-                                .putBoolean(PREF_SIGNED_IN_BEFORE, true)
-                                .apply();
+                        mGeoRentingService.auth(new User(getTokenResult.getToken()))
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .map(sessionToken -> {
+                                    Timber.d("Got Session Token!");
+                                    if (sessionToken.token == null || sessionToken.token.equals("")) {
+                                        return sessionToken;
+                                    }
 
-                        mApp.setSessionToken(sessionToken);
+                                    mPreferences.edit()
+                                            .putBoolean(PREF_SIGNED_IN_BEFORE, true)
+                                            .apply();
 
-                        return sessionToken;
+                                    mApp.setSessionToken(sessionToken);
+
+
+                                    return sessionToken;
+                                }).subscribe((sessionToken) -> {
+                                    subscriber.onNext(sessionToken);
+                                    subscriber.onCompleted();
+                                }, error -> {
+                                    subscriber.onError(error);
+                                    subscriber.onCompleted();
+                                });
                     });
+                });
+            });
         } else {
             return Observable.error(new Exception());
         }
     }
 
+    @DebugLog
     public boolean blockingSignIn() {
         if(mApp.getSessionToken() != null && !TextUtils.isEmpty(mApp.getSessionToken().token) && mApp.getSessionToken().user != null) return true;
 
@@ -100,8 +136,10 @@ public class GoogleAuth {
 
         if(r.isSuccess()) {
             if(r.getSignInAccount() == null) return false;
-            String authCode = r.getSignInAccount().getServerAuthCode();
-            if(authCode == null) return false;
+
+            Task<AuthResult> result = firebaseAuthWithGoogle(r.getSignInAccount());
+
+            String authCode = result.getResult().getUser().getToken(false).getResult().getToken();
 
             SessionToken sessionToken = mGeoRentingService.auth(new User(authCode)).toBlocking().first();
             mApp.setSessionToken(sessionToken);
@@ -110,6 +148,11 @@ public class GoogleAuth {
         }
 
         return false;
+    }
+
+    @DebugLog
+    private Task<AuthResult> firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        return mAuth.signInWithCredential(GoogleAuthProvider.getCredential(account.getIdToken(), null));
     }
 
     public void signOut(GoogleApiClient mGoogleClient) {
@@ -154,7 +197,8 @@ public class GoogleAuth {
     public GoogleSignInOptions getGoogleSignInOptions() {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
-                .requestServerAuthCode(mApp.getString(R.string.google_server_id), false)
+                .requestIdToken(mApp.getString(R.string.google_server_id))
+                //.requestServerAuthCode(mApp.getString(R.string.google_server_id), false)
                 .build();
         return gso;
     }
